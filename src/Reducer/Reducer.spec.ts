273151bea17aab1b2358f6ac410aec816a296d7e
hitmands - Giuseppe Mandato <g.mandato@elsevier.com>
2017-12-23 14:59:50 +0000

    feat($compile): Initial Scaffolding

---
+++ src/Reducer/Reducer.spec.ts
@@ -0,0 +1,1 @@
+import Reducer from './';
---
+++ src/redux-fluent.ts
@@ -0,0 +1,5 @@
+import Reducer from './Reducer/Reducer';
+import Action from './Action/Action';
+
+export { Reducer as createReducer };
+export { Action as createAction };
