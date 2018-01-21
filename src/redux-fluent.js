af4641765806eb048b3d995c9c06eaef216e0b9c
Giuseppe Mandato <gius.mand.developer@gmail.com>
2018-01-21 14:32:54 +0000

    feat(Reducer): add createCombinableReducers (#11)
    returns an object valid for redux#combineReducers

--- src/redux-fluent.js
+++ src/redux-fluent.js
@@ -1,2 +1,3 @@
 export { default as createAction } from './Action/Action';
 export { default as createReducer } from './Reducer/Reducer';
+export { default as createCombinableReducers } from './CombinableReducers/CombinableReducers';
