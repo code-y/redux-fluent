4dc3c277e03e6c985c8005a68997a1c170038502
hitmands Giuseppe Mandato <gius.mand.developer@gmail.com>
2018-01-22 08:20:14 +0000

    fix(Reducer): mv reducer.type reducer.domain

--- src/CombinableReducers/CombinableReducers.js
+++ src/CombinableReducers/CombinableReducers.js
@@ -1,6 +1,6 @@
 export default (...reducers) => reducers
   .reduce((res, reducer) => {
-    const domain = reducer.type || reducer.toString();
+    const domain = reducer.domain || reducer.toString();
 
     if (domain in res) {
       throw new TypeError(`[redux-fluent] Reducer "${domain}" was already registered.`);
