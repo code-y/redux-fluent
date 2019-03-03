3f8a4d8a99f2040f5dc4833f7270b6e7521d56c8
hitmands - Giuseppe Mandato <gius.mand.developer@gmail.com>
2019-03-03 14:54:44 +0000

    feat(redux-fluent): export new api

---
+++ src/redux-fluent.ts
@@ -0,0 +1,5 @@
+export { combineReduxFluentReducers as combineReducers } from './combine-reducers/combineReducers';
+export { createAction } from './create-action/createAction';
+export { createReducer } from './create-reducer/createReducer';
+export { ofType } from './of-type/ofType';
+export { withConfig } from './with-config/withConfig';
